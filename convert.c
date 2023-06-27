a114f213eec44f47cbd5c54863c7edddea199211
M9china <nqobilencaza2@gmail.com>
2023-06-27 13:33:26 -0700

    handling conversion specifiers

--- convert.c
+++ convert.c
@@ -4,48 +4,60 @@
  * checker - Function to check for format specifiers
  * @format: Pointer to string
  * @symbols: Array of format structure
- * @str: pointer to args.
- * Return: Number of char printed
+ * @str: Pointer to args
+ * Return: Number of characters printed
  */
-
 int checker(const char *format, format_t symbols[], va_list str)
 {
-	int i = 0, j, outcome, printed_chars = 0;
+	int i = 0;
+	int printed_chars = 0;
 
-	while (format && (*(format + i)))
+	while (format && format[i])
 	{
-		if (*(format + i) == '%')
+		if (format[i] == '%')
 		{
-			for (j = 0; symbols[j].sign != NULL; j++)
+			if (format[i + 1] != '\0')
 			{
-				if (*(format + (i + 1)) == symbols[j].sign[0])
+				int j = 0;
+				while (symbols[j].sign != NULL)
 				{
-					outcome = symbols[j].fun(str);
-					if (outcome == -1)
-						return (-1);
-					printed_chars += outcome;
-					break;
+					if (format[i + 1] == symbols[j].sign[0])
+					{
+						if (format[i + 1] == 'd' || format[i + 1] == 'i')
+						{
+							int value = va_arg(str, int);
+							printed_chars += _putint(value);
+						}
+						else
+						{
+							int outcome = symbols[j].fun(str);
+							if (outcome == -1)
+								return -1;
+							printed_chars += outcome;
+						}
+						break;
+					}
+					j++;
 				}
-			}
-			if (symbols[j].sign == NULL && *(format + (i + 1)) != ' ')
-			{
-				if (*(format + (i + 1)) != '\0')
+				if (symbols[j].sign == NULL && format[i + 1] != ' ')
 				{
-					_putstring(*(format + i));
-					_putstring(*(format + (i + 1)));
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
 					printed_chars += 2;
 				}
-				else
-					return (-1);
+				i++;
 			}
-			i += 1;
+			else
+				return -1;
 		}
 		else
 		{
-			_putstring(*(format + i));
+			_putchar(format[i]);
 			printed_chars++;
 		}
 		i++;
 	}
-	return (printed_chars);
+
+	return printed_chars;
 }
+
