0c3962fa64f144cf9c855559879398ddecdfd4b3
M9china <nqobilencaza2@gmail.com>
2023-06-28 06:33:38 -0700

    handling conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,51 +1,132 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
+/**
+ * _printf - prints output according to a format
+ * @format: the format string to be printed
+ *
+ * Return: the number of characters printed (excluding the null byte used
+ * to end output to strings), or -1 if an error occured
+ */
 int _printf(const char *format, ...)
 {
-    int count = 0; 
+	va_list args;
+	int count = 0;
+	int i;
 
-    va_list args;
-    va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++; 
-            
-            switch (*format) {
-                case 'c': {
-                    int c = va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char *);
-                    while (*s != '\0') {
-                        putchar(*s);
-                        s++;
-                        count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                    
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return count;
+	va_start(args, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					count += print_c(args);
+					break;
+				case 's':
+					count += print_string(args);
+					break;
+				case 'd':
+				case 'i':
+					count += print_number(args);
+					break;
+				case '%':
+					count += write(1, "%", 1);
+					break;
+				default:
+					count += write(1, &format[i - 1], 2);
+					break;
+			}
+		}
+		else
+		{
+			count += write(1, &format[i], 1);
+		}
+	}
+	va_end(args);
+	return (count);
 }
 
+/**
+ * print_c - prints a single character to stdout
+ * @args: va_list pointing to the character to print
+ *
+ * Return: the number of character written to stdout
+ */
+int print_c(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - prints a string to stdout
+ * @args: va_list pointing to the string to print
+ *
+ * Return: the number of characters written to stdout
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	return (write(1, str, _strlen(str)));
+}
+
+/**
+ * print_number - prints an integer to stdout
+ * @args: va_list pointing to the integer to print
+ *
+ * Return: the number of characters written to stdout
+ */
+int print_number(va_list args)
+{
+	int n = va_arg(args, int);
+	char buffer[20];
+	int i = 0, j, count = 0;
+
+	if (n == 0)
+	{
+		count += write(1, "0", 1);
+		return (count);
+	}
+	if (n < 0)
+	{
+		count += write(1, "-", 1);
+		n = -n;
+	}
+	while (n > 0)
+	{
+		buffer[i] = n % 10 + '0';
+		i++;
+		n /= 10;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		count += write(1, &buffer[j], 1);
+	}
+	return (count);
+}
+
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+	return (length);
+}
