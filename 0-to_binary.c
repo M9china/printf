78fee802516949882f562d42797abef9f3e7a4c4
M9china <nqobilencaza2@gmail.com>
2023-06-30 04:33:56 -0700

    created a function that changes a number to binary

---
+++ 0-to_binary.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+void print_binary(unsigned int num) {
+    unsigned int bit = 1 << (sizeof(unsigned int) * 8 - 1);
+    int printed = 0;
+
+    while (bit) {
+        if (num & bit)
+            putchar('1');
+        else
+            putchar('0');
+
+        bit >>= 1;
+        printed = 1;
+    }
+
+    if (!printed)
+        putchar('0');
+}
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    while (*format) {
+        if (*format == '%') {
+            format++;  // Move to the next character after '%'
+
+            if (*format == 'b') {
+                unsigned int num = va_arg(args, unsigned int);
+                print_binary(num);
+            } else {
+                putchar('%');  // Print '%' as it is
+                putchar(*format);
+            }
+        } else {
+            putchar(*format);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return 0;
+}
+
+int main(void) {
+    _printf("%b\n", 98);
+    return 0;
+}
+
