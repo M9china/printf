c3b54b35421c78be4b29cbbf65312816d0bee609
M9china <nqobilencaza2@gmail.com>
2023-06-29 17:03:24 -0700

    crated main header file

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * struct print_func - struct to store function and associated format char
+ * @f: function pointer to a function to print associated format char
+ * @format: associated format char
+ *
+ * Description: Used to select the correct function to print a format char
+ */
+typedef struct print_func
+{
+	int (*f)(va_list, char *);
+	char format;
+} print_func_t;
+
+/* prototypes */
+int _printf(const char *format, ...);
+
+
+#endif /* MAIN_H */
