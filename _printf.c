1817fd1e1d263a2ee4b90b60f5a59a3f8826bd25
M9china <nqobilencaza2@gmail.com>
2023-06-30 01:41:06 -0700

    hit fix

--- _printf.c
+++ _printf.c
@@ -9,17 +9,52 @@
  * or -1 if an error occured
  */
 
+void print_rev(char *s);
+
 int _printf(const char *format, ...)
 {
     va_list args;
-
-    int count;
-    
     va_start(args, format);
-
-    count = vprintf(format, args);
-
+    
+    int count = 0;
+    
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            
+            if (*format == 'r')
+            {
+                char *str = va_arg(args, char*);
+                print_rev(str);
+                int len = strlen(str);
+                count += len;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        
+        format++;
+    }
+    
     va_end(args);
     
     return count;
+}
+
+void print_rev(char *s)
+{
+    int i = 0;
+    
+    while (s[i])
+        i++;
+    
+    while (i--)
+        putchar(s[i]);
+    
+    putchar('\n');
 }
