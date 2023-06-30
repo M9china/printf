0c90d211d908ed41f55bc416edd19d89d65e5025
M9china <nqobilencaza2@gmail.com>
2023-06-30 01:47:03 -0700

    deleted recent changes

--- _printf.c
+++ _printf.c
@@ -9,52 +9,17 @@
  * or -1 if an error occured
  */
 
-void print_rev(char *s);
-
 int _printf(const char *format, ...)
 {
     va_list args;
+
+    int count;
+    
     va_start(args, format);
-    
-    int count = 0;
-    
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            
-            if (*format == 'r')
-            {
-                char *str = va_arg(args, char*);
-                print_rev(str);
-                int len = strlen(str);
-                count += len;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        
-        format++;
-    }
-    
+
+    count = vprintf(format, args);
+
     va_end(args);
     
     return count;
-}
-
-void print_rev(char *s)
-{
-    int i = 0;
-    
-    while (s[i])
-        i++;
-    
-    while (i--)
-        putchar(s[i]);
-    
-    putchar('\n');
 }
